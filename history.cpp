c44130311b1c4bd574350c96a42a847e4cab6bf5
barnaa <andrej.barna@centrum.sk>
2016-05-03 17:21:54 +0200

    Save Fix a inkrementacne/dekrementacne fluidum
    Ak by sa nieco dosieralo tak to robia riadky 26 a 27

--- history.cpp
+++ history.cpp
@@ -4,17 +4,15 @@
 
 /* Vrati nasledujici stav desky */
 const TState& History::nextState(){
-  it++;
-  if(it == states.end()) // Neni nasledujiciho stavu
+  if(++it == states.end()) // Neni nasledujiciho stavu
     it--;
   return (*it);
 }
 
 /* Vrati predchozi stav desky*/
 const TState& History::prevState(){
-  if(it == states.begin()) // Neni predchoziho stavu
-    return (*it);
-  it--;
+  if(it != states.begin()) // Neni predchoziho stavu
+    it--;
   return (*it);
 }
 
@@ -25,10 +23,8 @@ const TState& History::prevState(){
  */
 void History::storeState(Board board, TColor playerColor, const int blackScore, const int whiteScore){
   // Odstraneni vsech ulozenych stavu ktere jsou az za aktualni pozici v historii
-  it++;
-  if(states.end() != it)
-      states.erase(it,states.end());
-  it--;
+  if(states.end() != ++it)
+      states.erase(it--, states.end());
   states.push_back({board, playerColor, blackScore, whiteScore});
   it++;
 }
@@ -45,15 +41,14 @@ const std::string History::prepareToStore(){
   std::string res;
 
   std::list<TState>::iterator iter{states.begin()};
-  std::list<TState>::iterator niter{states.begin()};
-  niter++;
+  std::list<TState>::iterator niter{states.begin()++};
   int size = iter->board.getSize();
 
   bool breaker = false;
   while(niter != states.end()){
       for (int x = 0; x < size; x++) {
           for (int y = 0; y < size; y++){
-              if(iter->board.getStone(x, y) == NONE && niter->board.getStone(x, y) != NONE){
+              if(iter->board.getStone(x, y) == NONE && (niter->board.getStone(x, y) == BLACK || niter->board.getStone(x, y) == WHITE)){
                 res += static_cast<char>((x + 1)*16 + y + 1);
                 breaker = true;
                 break;
